7c2163e6d47b95f2d40e4a5822e7fef66bd89ea3
JackLovel <gogkatsu@outlook.com>
2021-10-04 21:12:56 +0800

    update

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -13,4 +13,28 @@ bmi h m = m / h^2
 mymax :: Integer -> Integer -> Integer
 mymax x y = if x > y then x else y
 
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial(n-1)
 
+fib :: Integer -> Integer
+fib n
+  | n == 0 = 1
+  | n == 1 = 1
+  | n > 1 = fib (n-1) + fib (n-2)
+
+mySum :: [Int] -> Int
+mySum []  = 0
+mySum (x:xs) = x + mySum xs
+
+-- 图书借阅管理
+
+-- 查看某本书的借阅者
+borrowers::DataBase -> Book -> [Borrower]
+borrowers db book = [p | (p,b) <- db, b == book]
+
+-- 某读者借了几本书
+
+exampleData = [("Alice", "Haskell: The Craft of Functional"),
+("Alice", "A river runs through it"),
+("Grats", "Haskell functional programming")]
